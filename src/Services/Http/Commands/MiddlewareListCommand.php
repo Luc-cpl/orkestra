35708a788f1155b1c9d0895f19eec98a40439460
GitHub Action <actions@github.com>
2024-07-22 15:48:53 +0000

    [ci-review] code fixer

--- src/Services/Http/Commands/MiddlewareListCommand.php
+++ src/Services/Http/Commands/MiddlewareListCommand.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+
 #[AsCommand(name: 'middleware:list')]
 class MiddlewareListCommand extends Command
 {
